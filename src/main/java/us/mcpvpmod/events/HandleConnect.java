662bc66c00570d5d0021ed8bbf6f53e05ecc2fba
NomNuggetNom <nomnuggetnom@gmail.com>
2014-09-18 09:06:52 -0400

    Update HandleConnect.java

--- src/main/java/us/mcpvpmod/events/HandleConnect.java
+++ src/main/java/us/mcpvpmod/events/HandleConnect.java
@@ -6,11 +6,7 @@ import cpw.mods.fml.common.network.FMLNetworkEvent.ClientConnectedToServerEvent;
 
 public class HandleConnect {
 
-	public static String send = "/59D8AE933F582F5441C6F5C8FA19CBA2";
-	
-	
 	public static void onConnect(ClientConnectedToServerEvent event) {
-
 	}
 	
 }
