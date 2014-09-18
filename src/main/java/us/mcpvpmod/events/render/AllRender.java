66300434d9c79e6357499e0bff3e35bc65ca08a8
NomNuggetNom <nomnuggetnom@gmail.com>
2014-09-18 09:12:46 -0400

    Added comments.

--- src/main/java/us/mcpvpmod/events/render/AllRender.java
+++ src/main/java/us/mcpvpmod/events/render/AllRender.java
@@ -6,13 +6,17 @@ import us.mcpvpmod.Server;
 import us.mcpvpmod.game.state.StateCTF;
 import us.mcpvpmod.gui.FriendsBlock;
 
-
+/**
+ * Render handling for all servers.
+*/
 public class AllRender {
 	
 	public static void onRender(RenderGameOverlayEvent event) {
-		if (Main.mc.isSingleplayer()) return;
+		// If we don't render during the TEXT phase, we'll screw up other displays due to OpenGL settings.
+		// TODO: fix OpenGL interference.
 		if (event.type != RenderGameOverlayEvent.ElementType.TEXT) return;
 		
+		// Display our FriendsBlock regardless of server.
 		FriendsBlock.display();
 	}
 	
