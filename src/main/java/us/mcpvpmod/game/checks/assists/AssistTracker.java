1ef9aa72e6e5983854c4193166a86b637777744a
NomNuggetNom <nomnuggetnom@gmail.com>
2014-09-18 10:16:29 -0400

    Added comments

--- src/main/java/us/mcpvpmod/game/checks/assists/AssistTracker.java
+++ src/main/java/us/mcpvpmod/game/checks/assists/AssistTracker.java
@@ -5,16 +5,25 @@ import java.util.ArrayList;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 
+/**
+ * Tracks hits and deaths to try and predict if you assisted in killing a player.
+ */ 
 public class AssistTracker {
 
 	public static ArrayList<AssistTracker> trackers = new ArrayList<AssistTracker>();
 	
+	/**
+	* The attack event, will add the player to a list.
+	*/
 	public static void onAttack(AttackEntityEvent event) {
 		for (AssistTracker tracker : trackers) {
 			AssistTracker.onAttack(event);
 		}
 	}
 	
+	/**
+	 * The death event, will check the list for a player and determine if it was an assist.
+	 */ 
 	public static void onDeath(LivingDeathEvent event) {
 		for (AssistTracker tracker : trackers) {
 			AssistTracker.onDeath(event);
