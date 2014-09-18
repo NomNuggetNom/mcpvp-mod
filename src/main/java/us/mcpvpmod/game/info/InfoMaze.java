daea45becdb23be71fab7b6423a41c330dadce13
NomNuggetNom <nomnuggetnom@gmail.com>
2014-09-18 10:29:16 -0400

    Update InfoMaze.java

--- src/main/java/us/mcpvpmod/game/info/InfoMaze.java
+++ src/main/java/us/mcpvpmod/game/info/InfoMaze.java
@@ -7,14 +7,18 @@ public class InfoMaze {
 
 	public static String reTime = "Starting in (.*)";
 	
+	/**
+	 * @return The time as an integer, e.g. 5:00 would return 500.
+	 */ 
 	public static int getTime() {
 		String boardTitle = BoardHelper.getBoardTitle();
-		if (boardTitle != null) {
-			String timeLeft = boardTitle.replaceAll(reTime, "$1");
-			if (timeLeft.replaceAll("\\D", "").equals("")) return -1;
-			return Integer.parseInt(timeLeft.replaceAll("\\D", ""));
-		}
-		return -1;
+		
+		// Sometimes there is no ScoreBoard, even in pre-game.
+		if (boardTitle == null) return -1;
+		
+		String timeLeft = boardTitle.replaceAll(reTime, "$1");
+		if (timeLeft.replaceAll("\\D", "").equals("")) return -1;
+		return Integer.parseInt(timeLeft.replaceAll("\\D", ""));
 	}
 	
 }
