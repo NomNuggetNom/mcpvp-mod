898d7d2168d1b185e0fb60898e7750f16d9fcff3
NomNuggetNom <nomnuggetnom@gmail.com>
2014-09-18 09:08:16 -0400

    Added comments

--- src/main/java/us/mcpvpmod/events/tick/AllTick.java
+++ src/main/java/us/mcpvpmod/events/tick/AllTick.java
@@ -6,14 +6,21 @@ import us.mcpvpmod.gui.Medal;
 import us.mcpvpmod.trackers.BoardTracker;
 import cpw.mods.fml.common.gameevent.TickEvent;
 
+/** 
+* Tick handling for all servers. 
+*/
 public class AllTick {
 	
 	public static void onTick(TickEvent event) {
+		// If we don't render at the end of the phase, we'll screw up other displays due to OpenGL settings.
 		if (event.type == TickEvent.Type.RENDER && event.phase == event.phase.END) {
+			// Show all Alerts and Medals
 			Alerts.alert.showAlerts();
 			Medal.showAll();
 		}
 		
+		// Update our board trackers because they don't rely on events
+		// TODO Make BoardTracker.updateAll()
 		for (BoardTracker tracker : BoardTracker.boardTrackers) {
 			tracker.update();
 		}
