3ebd00798837203124aa027df9207aa3ce340073
NomNuggetNom <nomnuggetnom@gmail.com>
2014-09-18 09:10:26 -0400

    Update TickBuild.java

--- src/main/java/us/mcpvpmod/events/tick/TickBuild.java
+++ src/main/java/us/mcpvpmod/events/tick/TickBuild.java
@@ -6,6 +6,9 @@ import cpw.mods.fml.common.gameevent.TickEvent;
 public class TickBuild {
 
 	public static void onTick(TickEvent event) {
+		// Update our variables.
+		// For build, this might not be needed, since all variables rely on ChatTrackers.
+		// TODO: Consider removing to improve performance.
 		VarsBuild.putVars();
 	}
 	
