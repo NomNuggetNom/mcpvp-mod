3ce5bf0387f913a31d9a7d5c81d7062b5c3295c7
NomNuggetNom <nomnuggetnom@gmail.com>
2014-09-18 15:32:25 -0400

    Added comments

--- src/main/java/us/mcpvpmod/ServerData.java
+++ src/main/java/us/mcpvpmod/ServerData.java
@@ -6,10 +6,16 @@ public class ServerData {
 
 	public static Minecraft mc = Minecraft.getMinecraft();
 	
+	/**
+	 * @return If we're on a server.
+	 */
 	public static boolean isMultiplayer() {
 		return (!mc.isSingleplayer() && mc.func_147104_D() != null);
 	}
 	
+	/**
+	 * @return The last server IP connected to.
+	 */ 
 	public static String serverIP() {
 		if (!isMultiplayer()) return "none";
 		return mc.func_147104_D().serverIP;
