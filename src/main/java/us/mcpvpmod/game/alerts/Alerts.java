9066f76c933deeaeec37ccc495dac39cd1fbc0d1
NomNuggetNom <nomnuggetnom@gmail.com>
2014-09-18 15:29:39 -0400

    Added comments

--- src/main/java/us/mcpvpmod/game/alerts/Alerts.java
+++ src/main/java/us/mcpvpmod/game/alerts/Alerts.java
@@ -3,8 +3,13 @@ package us.mcpvpmod.game.alerts;
 import us.mcpvpmod.Main;
 import us.mcpvpmod.gui.Alert;
 
+/**
+ * Contains the handle that all alerts use.
+ */ 
 public class Alerts {
-
+	
+	// ALL Alerts must reference this. 
+	// It is updated every tick to display alerts.
 	public static Alert alert = new Alert(Main.mc);
 	
 }
