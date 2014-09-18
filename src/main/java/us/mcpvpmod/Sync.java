c18003d45c570dee5dc5fb9d7b8dff59a9c6955d
NomNuggetNom <nomnuggetnom@gmail.com>
2014-09-18 09:03:08 -0400

    Added comments

--- src/main/java/us/mcpvpmod/Sync.java
+++ src/main/java/us/mcpvpmod/Sync.java
@@ -33,49 +33,46 @@ public class Sync {
 	public static void sync() {
 		FMLLog.info("[MCPVP] Syncing configurations.");
 
+		// Set formatting codes.
 		Format.setCodes();
-
+		// Clear the friends list.
+		FriendsList.clearList();
+		
+		// Sync MCPVP Configs
 		ConfigChat.syncConfig();
 		ConfigFriends.syncConfig();
 		ConfigHUD.syncConfig();
-
+		// Sync CTF Configs
 		ConfigCTFChat.syncConfig();
 		ConfigCTFAlerts.syncConfig();
 		ConfigCTFHUD.syncConfig();
 		ConfigCTFSounds.syncConfig();
-
-		ConfigChat.syncConfig();
-		ConfigFriends.syncConfig();
-		ConfigCTFChat.syncConfig();
-
+		// Sync Kit Configs
 		ConfigKitHUD.syncConfig();
 		ConfigKitAlerts.syncConfig();
 		ConfigKitSounds.syncConfig();
-
+		// Sync Maze Configs
 		ConfigMazeHUD.syncConfig();
 		ConfigMazeAlerts.syncConfig();
 		ConfigMazeSounds.syncConfig();
 		ConfigMazeSelect.syncConfig();
-		
+		// Sync Build Configs
 		ConfigBuildHUD.syncConfig();
 
-		FriendsList.clearList();
-
+		// Sync all InfoBlocks
 		InfoBlock.blocks.clear();
-
 		InfoBlock.createBlocks(ConfigKitHUD.render, Server.KIT, StateKit.PLAY);
-
 		InfoBlock.createBlocks(ConfigCTFHUD.renderPre, Server.CTF, StateCTF.PRE);
 		InfoBlock.createBlocks(ConfigCTFHUD.renderPlay, Server.CTF,StateCTF.PLAY);
 		InfoBlock.createBlocks(ConfigCTFHUD.renderPost, Server.CTF,StateCTF.POST);
-
 		InfoBlock.createBlocks(ConfigMazeHUD.renderPre, Server.MAZE,StateMaze.WAIT);
 		InfoBlock.createBlocks(ConfigMazeHUD.renderPre, Server.MAZE,StateMaze.PRE);
 		InfoBlock.createBlocks(ConfigMazeHUD.renderPlay, Server.MAZE,StateMaze.PLAY);
 		InfoBlock.createBlocks(ConfigMazeHUD.renderPost, Server.MAZE,StateMaze.DEAD);
-		
 		InfoBlock.createBlocks(ConfigBuildHUD.render, Server.BUILD, DummyState.NONE);
 
+
+		// Sync cores, which are responsible for setting triggers and trackers.
 		CoreCTF.setup();
 		CoreKit.setup();
 		CoreMaze.setup();
