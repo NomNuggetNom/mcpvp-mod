0b8e3415f16ffb42cd0504942d68f647fe1894db
NomNuggetNom <NomNuggetNom@gmail.com>
2014-09-21 12:24:32 -0400

    Added comments

--- src/main/java/us/mcpvpmod/game/vars/Vars.java
+++ src/main/java/us/mcpvpmod/game/vars/Vars.java
@@ -6,10 +6,20 @@ public class Vars {
 
 	public static HashMap<String, String> vars = new HashMap<String, String>();
 	
+	/**
+	 * Puts a value into the variable storage.
+	 * @param key The key of the value.
+	 * @param value The value.
+	 */
 	public static void put(String key, String value) {
 		vars.put(key, value);
 	}
 	
+	/**
+	 * Returns a stored string value.
+	 * @param key The key of the value to return.
+	 * @return The stored value of the key.
+	 */
 	public static String get(String key) {
 		if (!vars.containsKey(key)) {
 			return "";
@@ -17,6 +27,11 @@ public class Vars {
 		return vars.get(key);
 	}
 	
+	/**
+	 * Returns a stored int value.
+	 * @param key The key of the value to return.
+	 * @return The stored value of the key.
+	 */
 	public static int getInt(String key) {
 		if (!vars.containsKey(key)) {
 			return -1;
@@ -24,6 +39,9 @@ public class Vars {
 		return Integer.parseInt(vars.get(key));
 	}
 	
+	/**
+	 * Prints out every stored value. Useful for debugging.
+	 */
 	public static void print() {
 		for (String key : vars.keySet()) {
 			System.out.println(key + "=" + vars.get(key));
