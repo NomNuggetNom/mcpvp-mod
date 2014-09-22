ddb93fbf31e915b82a20428c5296a3069a78e12a
NomNuggetNom <NomNuggetNom@gmail.com>
2014-09-22 19:11:32 -0400

    Added HG Kits

---
+++ src/main/java/us/mcpvpmod/game/kits/KitsHG.java
@@ -0,0 +1,125 @@
+package us.mcpvpmod.game.kits;
+
+import java.util.ArrayList;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class KitsHG {
+	
+	public static void putKits() {
+		new KitHG("Flash", Blocks.redstone_torch);
+		new KitHG("Digger", Blocks.dragon_egg);
+		new KitHG("Spy", Items.compass);
+		new KitHG("Specialist", Items.experience_bottle);
+		new KitHG("Tarzan", Blocks.vine);
+		new KitHG("Camel", Item.getItemById(373)); // water bottle
+		new KitHG("Acrobat", Items.record_blocks);
+		new KitHG("Nightlock", Items.mushroom_stew);
+		new KitHG("Kaya", Blocks.grass);
+		new KitHG("CookieMonster", Items.cookie);
+		new KitHG("Scorch", Items.leather_boots);
+		new KitHG("Frosty", Items.snowball);
+		new KitHG("Castaway", Blocks.dispenser);
+		new KitHG("Viper", Items.spider_eye);
+		new KitHG("Copycat", Blocks.yellow_flower);
+		new KitHG("Monster", new ItemStack(Items.skull, 1, 2));
+		new KitHG("Phantom", Items.feather);
+		new KitHG("Thermo", Blocks.daylight_detector);
+		new KitHG("Shapehshifter", Blocks.redstone_block);
+		new KitHG("Fireman", Items.water_bucket);
+		new KitHG("Summoner", Items.egg);
+		new KitHG("Snail", new ItemStack(Items.potionitem, 1, 10)); // slowness potion
+		new KitHG("Surprise", Items.name_tag);
+		new KitHG("Array", Blocks.beacon);
+		new KitHG("Stomper", Items.iron_boots);
+		new KitHG("Achillies", Items.wooden_sword);
+		new KitHG("Warder", Items.book);
+		new KitHG("Neo", Items.diamond_chestplate);
+		new KitHG("Chameleon", Items.wheat);
+		new KitHG("Miner", Items.stone_pickaxe);
+		new KitHG("Launcher", Blocks.sponge);
+		new KitHG("Switcher", Items.snowball);
+		new KitHG("Gambler", Blocks.stone_button);
+		new KitHG("Endermage", Blocks.portal);
+		new KitHG("Milkman", Items.milk_bucket);
+		new KitHG("Backup", Items.book);
+		new KitHG("Supressor", Items.arrow);
+		new KitHG("Scout", new ItemStack(Items.potionitem, 1, 16418)); //speed 2 potion
+		new KitHG("Vulture", Items.book);
+		new KitHG("Checkpoint", Blocks.flower_pot);
+		new KitHG("Cultivater", Blocks.sapling);
+		new KitHG("Thor", Items.wooden_axe);
+		new KitHG("Hermit", Items.mushroom_stew);
+		new KitHG("Jumper", Items.ender_pearl);
+		new KitHG("Archer", Items.bow);
+		new KitHG("Hunter", Items.porkchop);
+		new KitHG("Vampire", new ItemStack(Items.potionitem, 1, 5)); //Potion of instant health
+		new KitHG("Woolymorph", Blocks.wool);
+		new KitHG("Spiderman", Items.snowball);
+		new KitHG("Fisherman", Items.fishing_rod);
+		new KitHG("Chemist", new ItemStack(Items.potionitem, 1, 16420)); // posion 2 pot
+		new KitHG("Gladiator", Blocks.iron_bars);
+		new KitHG("Seeker", Items.ender_eye);
+		new KitHG("Trapper", Items.string);
+		new KitHG("Gravedigger", Items.iron_shovel);
+		new KitHG("Wisp", Items.magma_cream);
+		new KitHG("Doctor", Items.shears);
+		new KitHG("Rogue", Items.blaze_rod);
+		new KitHG("Anchor", Blocks.iron_block);
+		new KitHG("Pickpocket", Items.blaze_rod);
+		new KitHG("Lifeline", Items.lead);
+		new KitHG("Jackhammer", Items.stone_axe);
+		new KitHG("Explorer", Items.map);
+		new KitHG("Souper", new ItemStack(Items.dye, 1, 15)); // bonemeal
+		new KitHG("Beastmaster", new ItemStack(Items.spawn_egg, 1, 95)); // spawn egg something
+		new KitHG("Decoy", Blocks.wool);
+		new KitHG("Tank", Blocks.tnt);
+		new KitHG("Cannibal", new ItemStack(Items.spawn_egg, 1, 98)); // spawn ocelot
+		new KitHG("Worm", Items.lead);
+		new KitHG("Grenadier", Blocks.furnace);
+		new KitHG("Urgal", new ItemStack(Items.potionitem, 1, 9)); // strength pot
+		new KitHG("Analyzer", Items.ender_eye);
+		new KitHG("Rhino", Items.chainmail_helmet);
+		new KitHG("Dwarf", Items.diamond_axe);
+		new KitHG("Backpacker", Blocks.chest);
+		new KitHG("Crafter", Blocks.crafting_table); // actually a nether star, but I make my own rules
+		new KitHG("Salvager", Blocks.anvil);
+		new KitHG("Grandpa", Items.stick);
+		new KitHG("Timelord", Items.clock);
+		new KitHG("Grappler", Items.lead);
+		new KitHG("Demoman", Blocks.stone_pressure_plate);
+		new KitHG("Hulk", Blocks.dispenser);
+		new KitHG("Necro", Items.bone);
+		new KitHG("Redstoner", Blocks.piston);
+		new KitHG("Turtle", Items.diamond_chestplate);
+		new KitHG("Rider", Items.iron_horse_armor);
+		new KitHG("Kangaroo", Items.fireworks);
+		new KitHG("Madman", new ItemStack(Items.potionitem, 1, 8)); //weakness potion
+		new KitHG("Santa", Items.written_book);
+		new KitHG("Reaper", Items.wooden_hoe);
+		new KitHG("Burrower", Items.slime_ball);
+		new KitHG("Hades", Blocks.iron_bars);
+		new KitHG("Jellyfish", Blocks.water);
+		new KitHG("Spectre", Blocks.redstone_wire);
+		new KitHG("Pyro", Items.flint_and_steel);
+		new KitHG("Spellmaster", Items.lead);
+		new KitHG("Plague", Items.ghast_tear);
+		new KitHG("Berserker", Blocks.fire);
+		new KitHG("Boxer", Items.stone_sword);
+		new KitHG("Viking", Items.iron_axe);
+		new KitHG("Soulstealer", Blocks.soul_sand);
+		new KitHG("Levitator", Blocks.torch);
+		new KitHG("Blink", Items.nether_star);
+		new KitHG("Ninja", new ItemStack(Blocks.wool, 1, 15)); // Black wool
+		new KitHG("Assassin", Items.golden_sword);
+		new KitHG("Werewolf", Items.clock);
+		new KitHG("Forger", Items.coal);
+		new KitHG("Lumberjack", Items.wooden_axe);
+		new KitHG("Barbarian", Items.wooden_sword); // empty wooden sword
+		new KitHG("Poseidon", Blocks.water);
+		new KitHG("None", Blocks.web);
+	}
+}
