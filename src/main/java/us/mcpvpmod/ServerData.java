2a724e76328f51ca33f4070d652348941008bd34
NomNuggetNom <NomNuggetNom@gmail.com>
2014-09-21 20:57:52 -0400

    Added methods for getting all players on aserver

--- src/main/java/us/mcpvpmod/ServerData.java
+++ src/main/java/us/mcpvpmod/ServerData.java
@@ -1,6 +1,11 @@
 package us.mcpvpmod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiPlayerInfo;
+import net.minecraft.entity.player.EntityPlayer;
 
 public class ServerData {
 
@@ -19,6 +24,25 @@ public class ServerData {
 	public static String serverIP() {
 		if (!isMultiplayer()) return "none";
 		return mc.func_147104_D().serverIP;
+	}
+	
+	/**
+	 * @return A list of all GuiPlayerInfo
+	 */
+	public static List<GuiPlayerInfo> getPlayersGui() {
+		if (Main.mc.thePlayer == null) return new ArrayList<GuiPlayerInfo>();
+		return Main.mc.thePlayer.sendQueue.playerInfoList;
+	}
+	
+	/**
+	 * @return A list of all GuiPlayerInfo
+	 */
+	public static List<EntityPlayer> getPlayers() {
+		List<EntityPlayer> players = new ArrayList<EntityPlayer>();
+		for (GuiPlayerInfo player : getPlayersGui()) {
+			players.add(Main.mc.theWorld.getPlayerEntityByName(player.name));
+		}
+		return players;
 	}
 
 }
