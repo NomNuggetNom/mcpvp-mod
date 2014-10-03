466730ac99e3390ebbe27f9761afdfe31b770534
NomNuggetNom <NomNuggetNom@gmail.com>
2014-10-03 15:07:34 -0400

    Finished moveable ArmorDisplay

--- src/main/java/us/mcpvpmod/gui/ArmorDisplay.java
+++ src/main/java/us/mcpvpmod/gui/ArmorDisplay.java
@@ -11,6 +11,7 @@ import us.mcpvpmod.Main;
 import us.mcpvpmod.config.all.ConfigHUD;
 import us.mcpvpmod.gui.info.ISelectable;
 import us.mcpvpmod.gui.info.Selectable;
+import us.mcpvpmod.util.Data;
 
 public class ArmorDisplay implements ISelectable {
 
@@ -49,13 +50,17 @@ public class ArmorDisplay implements ISelectable {
 		getArmor();
 		EntityClientPlayerMP player = Main.mc.thePlayer;
 
-
 		ScaledResolution res = new ScaledResolution(Main.mc, Main.mc.displayWidth, Main.mc.displayHeight);
-		y = 10;
-		int dispY = y;
-		x = res.getScaledWidth() - 20; // getStringWidth()
 		
-		// Cycle through "backwards" so that the helm is rendered first and the boots last.
+		x = (Data.get("ArmorDisplay.x") != null) ? 
+				Integer.parseInt(Data.get("ArmorDisplay.x")) 
+				: res.getScaledWidth() - itemSize - getStringWidth() - 10;
+				
+		y = (Data.get("ArmorDisplay.y") != null) ? 
+				Integer.parseInt(Data.get("ArmorDisplay.y")) 
+				: 10;	
+		int dispY = y;
+		
 		for (ItemStack item : items) {
 			dispItem(item, x, dispY);
 			dispY += itemSize;
@@ -84,14 +89,16 @@ public class ArmorDisplay implements ISelectable {
 		// Draw the item.
 		Draw.item(item, x, y);
 		
+		/*
 		Draw.rect(x - getStringWidth() - 2, 
 				y+2,
 				Main.mc.fontRenderer.getStringWidth(text) + itemSize + padding*2, 
 				itemSize, 
 				0, 0, 0, (float) 0.42/2);
+				*/
 		
 		// Draw the durability.
-		Draw.string(text, x - Main.mc.fontRenderer.getStringWidth(text) - 2, y+5, 0xFFFFFF, true);
+		Draw.string(text, x + itemSize + 2, y+5, 0xFFFFFF, true);
 		
 
 		
@@ -131,29 +138,65 @@ public class ArmorDisplay implements ISelectable {
 	public void drawOutline() {
 		// Base box
 		/*
-		Draw.rect(x - getStringWidth() - 2, 
-				y + 2, 
+		Draw.rect(x, 
+				y, 
 				w, 
 				h, 
 				1, 0, 0, 1);
 				*/
 		
-		Draw.rect(x - getStringWidth() - padding*2, 
-				y + 2, 
+		
+		Draw.rect(x, 
+				y - padding, 
+				w, 
 				padding, 
-				h, 
 				1, 0, 0, 1);
 		
-		Draw.rect(x - getStringWidth() - 3 - padding, 
-				y - padding, 
-				w + padding*2, 
+		Draw.rect(x, 
+				y + h, 
+				w, 
 				padding, 
+				1, 0, 0, 1);
+		
+		Draw.rect(x - padding, 
+				y - padding, 
+				padding, 
+				h + padding*2, 
+				1, 0, 0, 1);
+		
+		Draw.rect(x + w, 
+				y - padding, 
+				padding, 
+				h + padding*2, 
 				1, 0, 0, 1);
 	}
 
 	@Override
-	public void move(char direction, int pixels, boolean ctrl) {
-		// TODO Auto-generated method stub
+	public void move(char direction, int moveBy, boolean ctrl) {
+		ScaledResolution res = new ScaledResolution(Main.mc, Main.mc.displayWidth, Main.mc.displayHeight);
+		
+		// Holding CTRL will snap the box to the edges of the screen.
+		if (ctrl) {
+			if (direction == 'l') x = 0 + padding;
+			
+			if (direction == 'r') x = res.getScaledWidth() - this.w - padding;
+			
+			if (direction == 'u') y = 0 + padding;
+			
+			if (direction == 'd') y = res.getScaledHeight() - this.h - padding;
+		} else {
+			// Move left
+			if (direction == 'l') x -= moveBy;
+			// Move right
+			if (direction == 'r') x += moveBy;
+			// Move up
+			if (direction == 'u') y -= moveBy;
+			// Move down
+			if (direction == 'd') y += moveBy;
+		}
+		
+		Data.put("ArmorDisplay.x", "" + x);
+		Data.put("ArmorDisplay.y", "" + y);
 	}
 	
 }
