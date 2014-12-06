8fd6da144854b4615b1d13575b4c037ea30bdb23
NomNuggetNom <nomnuggetnom@gmail.com>
2014-12-06 13:16:26 -0500

    Reset IP on disconnect

--- src/main/java/us/mcpvpmod/events/HandleDisconnect.java
+++ src/main/java/us/mcpvpmod/events/HandleDisconnect.java
@@ -1,10 +1,12 @@
 package us.mcpvpmod.events;
 
+import us.mcpvpmod.ServerHelper;
 import cpw.mods.fml.common.network.FMLNetworkEvent.ClientDisconnectionFromServerEvent;
 
 public class HandleDisconnect {
 	
 	public static void onDisconnect(@SuppressWarnings("unused") ClientDisconnectionFromServerEvent event) {
+		ServerHelper.currentIP = "";
 	}
 
 }
